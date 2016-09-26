ac38c409205e26cfe3149b0266ddba50bde49958
Alexander Poluektov <alexander.poluektov@gmail.com>
2016-09-26 06:14:33 +0200

    Copiright 2016

--- life.py
+++ life.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 Alexander Poluektov (alexander.poluektov@gmail.com)
+# Copyright (c) 2011-2016 Alexander Poluektov (alexander.poluektov@gmail.com)
 #
 # Use, modification and distribution are subject to the MIT license
 # (See accompanying file MIT-LICENSE)
