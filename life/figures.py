4c20e244dd317933c7260eb231869ca1eb2fbc28
Alexander Poluektov <alexander.poluektov@gmail.com>
2016-10-04 07:45:24 +0200

    User able to choose a start figure and list available figures from command line
    Use '--figure <figure-name>' to select the start figure.
    Use '--list-figures' to print the list of all available figures.

--- life/figures.py
+++ life/figures.py
@@ -5,54 +5,88 @@
 
 from life.board import *
 
-def add_figure(board, f):
-    for x,y in parse(f):
+def add_figure(board, name):
+    f = catalog[name]
+    for x,y in f.cells:
         board.add_cell(x,y)
+
+
+def parse(s):
+    cells = []
+    lines = s.strip().split('\n')
+    for y,l in enumerate(lines):
+        l = ''.join(l.split())
+        for x,c in enumerate(l):
+            if c == 'X':
+                cells.append((x,y))
+    return cells
+
+
+catalog = dict()
+
+class Figure(object):
+    def __init__(self, name, text_repr, alt=None):
+        self.alt = alt
+        if not self.alt:
+            self.alt = []
+        self.name = name
+        self.cells = parse(text_repr)
+        catalog[self.name] = self
+        for n in self.alt:
+            catalog[n] = self
+
+def list_figures():
+    res = []
+    for k,v in catalog.items():
+        res.append((k,v.alt))
+    return res
+
 
 # stillife
 
-block = '''
+Figure('block', '''
 X X
 X X
-'''
+''')
 
-beehive = '''
+Figure('behive', '''
 - X -
 X - X
 X - X
 - X -
-'''
+''')
 
-loaf = '''
+Figure('loaf', '''
 - X X -
 X - - X
 - X - X
 - - X -
-'''
+''')
 
-boat = '''
+Figure('boat', '''
 X X -
 X - X
 - X -
-'''
+''')
+
 
 # oscillators
 
-blinker = 'X X X'
+Figure('blinker', 'X X X')
 
-toad = '''
+Figure('toad', '''
 - X X X
 X X X -
-'''
+''')
 
-beacon = '''
+Figure('beacon', '''
 X X - -
 X X - -
 - - X X
 - - X X
-'''
+''')
 
-pulsar = '''
+Figure('pulsar', '''
 - - X X X - - - X X X - -
 - - - - - - - - - - - - -
 X - - - - X - X - - - - X
@@ -66,9 +100,9 @@ X - - - - X - X - - - - X
 X - - - - X - X - - - - X
 - - - - - - - - - - - - -
 - - X X X - - - X X X - -
-'''
+''')
 
-pentadecathlon = '''
+Figure('pentadecathlon', '''
 - X -
 - X -
 X - X
@@ -79,48 +113,47 @@ X - X
 X - X
 - X -
 - X -
-'''
+''')
 
 # spaceships
 
-glider = '''
-- X _
+Figure('glider', '''
+- X -
 X - -
 X X X
-'''
+''')
 
-lightweight_spaceship = '''
+Figure('lightweight-spaceship', '''
 X - - X -
 - - - - X
 X - - - X
 - X X X X
-'''
-
-lwss = lightweight_spaceship
+''',
+alt=['lwss'])
 
 # complex life
 
-r_pentomino = '''
+Figure('r-pentomino', '''
 - X X
 X X -
 - X -
-'''
+''')
 
-diehard = '''
+Figure('diehard', '''
 - - - - - - X -
 X X - - - - - -
 - X - - - X X X
-'''
+''')
 
-acorn = '''
+Figure('acorn', '''
 - X - - - - -
 - - - X - - -
 X X - - X X X
-'''
+''')
 
 # glider guns
 
-glider_gun = '''
+Figure('glider-gun', '''
 - - - - - - - - - - - - - - - - - - - - - - - - X - - - - - - - - - - -
 - - - - - - - - - - - - - - - - - - - - - - X - X - - - - - - - - - - -
 - - - - - - - - - - - - X X - - - - - - X X - - - - - - - - - - - - X X
@@ -130,11 +163,11 @@ X X - - - - - - - - X - - - X - X X - - - - X - X - - - - - - - - - - -
 - - - - - - - - - - X - - - - - X - - - - - - - X - - - - - - - - - - -
 - - - - - - - - - - - X - - - X - - - - - - - - - - - - - - - - - - - -
 - - - - - - - - - - - - X X - - - - - - - - - - - - - - - - - - - - - -
-'''
+''')
 
 # my custom favorite
 
-collision = '''
+Figure('collision', '''
 - X - - - - - - - - - - -
 - - X - - - - - - - - - -
 X X X - - - X - - - - - -
@@ -150,15 +183,4 @@ X - - X - - - - - X - - X
 - - - - - X - X - - - - -
 - - - - - X - X - - - - -
 - - - - - - X - - - - - -
-'''
-
-
-def parse(s):
-    cells = []
-    lines = s.strip().split('\n')
-    for y,l in enumerate(lines):
-        l = ''.join(l.split())
-        for x,c in enumerate(l):
-            if c == 'X':
-                cells.append((x,y))
-    return cells
+''')
--- main.py
+++ main.py
@@ -6,21 +6,44 @@
 import life
 import life.figures
 import view
+import sys
 
 import argparse
 
 argparser = argparse.ArgumentParser(description='Conway\'s Game of Life sumulation.')
 argparser.add_argument('--pause', action="store_true", help='start game in pause mode (default: false)')
 argparser.add_argument('--code', default='B3/S23', help='born/survives game code (default: B3/S23)')
+argparser.add_argument('--figure', default='collision', help='initial figure on the board')
+argparser.add_argument('--list-figures', action='store_true', help='list availiable figures to use with --figure flag')
+
 
 def main():
     args = argparser.parse_args()
+    if args.list_figures:
+        print_figure_list(life.figures.list_figures())
+        sys.exit(0)
     board = life.Board(args.code)
-    life.figures.add_figure(board, life.figures.collision)
+    try:
+        life.figures.add_figure(board, args.figure)
+    except KeyError as detail:
+        print 'No such figure in catalog: %s' % detail
+        sys.exit(1)
     s = life.Style()
     v = view.View(board, s, 640, 480, 3)
     v.pause = args.pause
     v.run(200)
+
+
+def print_figure_list(figures):
+    for name, alt in figures:
+        print name,
+        if alt:
+            print '(',
+            for n in alt:
+                print n,
+            print ')'
+        if not alt:
+            print
 
 
 if __name__ == '__main__':
